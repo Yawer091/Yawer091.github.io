6f4ed7b4a85fb96d44070c33e51b1a7bad14c147
Yawer091 <yaweratique90@gmail.com>
2024-02-18 01:28:30 +0530

    8

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -50,7 +50,7 @@ export default function Header() {
           <button
             id="resume-button-2"
             onClick={downloadResume}
-            className="w-full md:w-auto pt-4 pl-8 pr-8 pb-4 text-3xl  bg-blue-500 text-white font-semibold rounded-lg mb-4 md:mb-0 hover:{bg-slate-300 transition ease-in-out delay-150 bg-blue-500} hover:-translate-y-1 hover:scale-110 hover:bg-blue-300 duration-300"
+            className="w-full md:w-auto pt-4 pl-8 pr-8 pb-4 text-3xl bg-blue-500 text-white font-semibold rounded-lg mb-4 md:mb-0 hover:{bg-slate-300 transition ease-in-out delay-150 bg-blue-500} hover:-translate-y-1 hover:scale-110 hover:bg-blue-300 duration-300"
           >
             Resume
           </button>
