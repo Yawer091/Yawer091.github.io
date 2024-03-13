62c0e49a63d01080ebe36104463a56d08c89d77f
Yawer091 <yaweratique90@gmail.com>
2024-03-13 16:22:04 +0530

    done

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     const resumeUrl = "/RESUME.pdf";
     const link = document.createElement("a");
     link.href = resumeUrl;
-    link.download = "/Resume.pdf";
+    link.download = "/RESUME.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
